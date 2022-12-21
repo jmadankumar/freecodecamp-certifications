fa7195a0e738881c0c7c53a4c2150740920df939
Madan Kumar <kumarmadan.j@gmail.com>
2022-12-21 18:38:21 +0530

    build a periodic table database project

---
+++ Relational Database/Build a Periodic Table Database/element.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT_DETAIL=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number='$1'")
+  else
+    ELEMENT_DETAIL=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE name='$1' OR symbol='$1'")
+  fi
+
+  if [[ -z $ELEMENT_DETAIL ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo $ELEMENT_DETAIL | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
+    do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done
+  fi 
+
+fi
